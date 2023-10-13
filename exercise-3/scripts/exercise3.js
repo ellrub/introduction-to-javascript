ead94a3b5ba32a8d3a1d691e97f39b164c764454
Ruben Ellefsen <ellefsen88@gmail.com>
2023-10-13 16:07:34 +0200

    Add myFunction with array

---
+++ exercise-3/scripts/exercise3.js
@@ -0,0 +1,30 @@
+alert("Hello there")
+
+(function myFunction() {
+    const myName = "Ruben"
+    let myAge = 34
+    let myArray = ["Pizza", "Hamburger", "Mango"]
+    let movieArray = [
+        {
+            name: "Lord of the Rigns",
+            credits: {
+                director: "Peter Jackson",
+                star: "Viggo Mortensen"
+            }
+        },
+        {
+            name: "Transformers",
+            credits: {
+                director: "Michael Bay",
+                star: "Shia Lebouf"
+            }
+        },
+        {
+            name: "Some movie",
+            credits: {
+                director: "Some guy",
+                star: "Some women"
+            }
+        }
+    ] 
+})();
